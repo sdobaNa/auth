8906681e23927aff71b4b66a180ea8e9c546f6d2
Vadim <you@example.com>
2021-10-08 10:14:29 +0500

    Initial Commit

---
+++ src/test/kotlin/ru/cobalt42/auth/AuthApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.cobalt42.auth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
