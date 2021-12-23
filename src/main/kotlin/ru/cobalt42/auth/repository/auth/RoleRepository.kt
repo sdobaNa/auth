ea211afa1144f681056e72df33172fb6cdad8dcc
Евгений Стовба <stovba_e@ptspec.ru>
2021-12-23 11:49:35 +0500

    fix

--- src/main/kotlin/ru/cobalt42/auth/repository/auth/RoleRepository.kt
+++ src/main/kotlin/ru/cobalt42/auth/repository/auth/RoleRepository.kt
@@ -5,10 +5,9 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.mongodb.repository.MongoRepository
 import ru.cobalt42.auth.model.role.Role
-import ru.cobalt42.auth.model.user.User
 
 interface RoleRepository : MongoRepository<Role, ObjectId> {
     fun findByUid(uid: String): Role
-    fun findByNameContainingIgnoreCase(name: String, padding: Pageable): Page<User>
+    fun findByNameContainingIgnoreCase(name: String, padding: Pageable): Page<Role>
     fun deleteByUid(uid: String)
 }
